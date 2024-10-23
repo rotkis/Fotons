a664b0c8028ff04bac7ccc5073953a0cb1b1b787
rotkis <rotkisarthur@gmail.com>
2024-10-22 21:19:48 -0300

    adicionar model

---
+++ Fotons/src/model/EstadoQuantico.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author manga
+ */
+public class EstadoQuantico {
+    private double larqura, a, b;
+    private int Ni,Nf;
+
+    public EstadoQuantico(double larqura, double a, double b, int Ni, int Nf) {
+        this.larqura = larqura;
+        this.a = a;
+        this.b = b;
+        this.Ni = Ni;
+        this.Nf = Nf;
+    }
+    
+    
+}
